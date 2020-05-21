3791dc290e0cb67a5de39c5d2529d91f21837faa
ItzDxniel <65725410+ItzDxniel@users.noreply.github.com>
2020-05-21 17:22:41 +0100

    Update itzhub.py

--- itzhub.py
+++ itzhub.py
@@ -1,54 +1,37 @@
-import smtplib
+import csv
 import sys
 
-
-class bcolors:
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
+def main():
+   menu()
 
 
-def banner():
-    print(bcolors.GREEN + '+[+[+[ Email-Bomber v1.0 ]+]+]+')
-    print(bcolors.GREEN + '+[+[+[ made with codes ]+]+]+')
-    print(bcolors.GREEN + '''
-	                    
-						
-                      ItzHub
-   
-                                       Author: ItzDxniel
-*.______________________________________________________________,'
-                                                                 ''')
+def menu():
+    print("************Welcome to ItzHub**************")
+    print()
 
-def main(start):
+    choice = input("""
+                     [1]: Please Register
+                     [2]: Login
+                     [2]: Logout
 
-choice ='0'
-while choice =='0':
-    print(b.colors.RED + '\n+[+[+[ Main Choice: Choose 1 of 5 choices ]+]+]')
-    print("Choose 1 for something")
-    print("Choose 2 for something")
-    print("Choose 3 for something")
-    print("Choose 4 for something")
-    print("Choose 5 to go to another menu")
+                      Please enter your choice: """)
 
-    choice = input ("Please make a choice: ")
-
-    if choice == "5":
-        print("Go to another menu")
-        second_menu()
-    elif choice == "4":
-        print("Do Something 4")
-    elif choice == "3":
-        print("Do Something 3")
-    elif choice == "2":
-        print("Do Something 2")
-    elif choice == "1":
-        print("Do Something 1")
+    if choice == "1" or choice =="1":
+        register()
+    elif choice == "2" or choice =="2":
+        login()
+    elif choice=="1" or choice=="2":
+        sys.exit
     else:
-        print("I don't understand your choice.")
+        print("You must only select one")
+        print("Please try again")
+        menu()
 
- def second_menu():
-     print("This is the second menu")
-
- main()
-			
+def register():
+   pass
+    
+def login():
+   pass
+    
+#the program is initiated, so to speak, here
+main()
